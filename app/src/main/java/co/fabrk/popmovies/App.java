70a978be014b1d420c6ece8e7c4bf86d2fec2455
jdesesquelles <jdesesquelles@gmail.com>
2016-04-05 07:15:43 +0200

    Enabled network inspection in app.java

--- app/src/main/java/co/fabrk/popmovies/App.java
+++ app/src/main/java/co/fabrk/popmovies/App.java
@@ -1,7 +1,10 @@
 package co.fabrk.popmovies;
 
 import android.app.Application;
+
 import com.facebook.stetho.Stetho;
+import okhttp3.OkHttpClient;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 /**
  * Created by ebal on 05/04/16.
@@ -10,5 +13,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
+        OkHttpClient client = new OkHttpClient();
+        client.networkInterceptors().add(new StethoInterceptor());
     }
 }
